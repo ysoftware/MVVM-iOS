8f1916f6549c5ed16f47d6c209de3f9d5d83d348
Yaroslav Erohin <ysoftware@users.noreply.github.com>
2018-07-13 16:45:49 +0300

    Стандартный класс array delegate

--- MVVM/Array View Model/ArrayDelegate.swift
+++ MVVM/Array View Model/ArrayDelegate.swift
@@ -5,7 +5,7 @@
 //  Created by ysoftware on 07.07.2018.
 //
 
-import Foundation
+import UIKit
 
 public protocol ArrayViewModelDelegate: class {
 
@@ -21,7 +21,47 @@ public protocol ArrayViewModelDelegate: class {
 public extension ArrayViewModelDelegate {
 
 	func didChangeState(to state:ArrayViewModelState) {}
+}
 
-	func didUpdateData<M, VM, Q>(_ arrayViewModel: ArrayViewModel<M, VM, Q>,
-								 _ update: Update) where Q:Query {}
+// MARK: - Простейший делегат для array view model для управления tableView / collectionView.
+
+public class DefaultArrayViewModelDelegate: ArrayViewModelDelegate {
+
+	private weak var tableView:UITableView?
+	private weak var collectionView:UICollectionView?
+
+	public var section = 0
+
+	public init(with tableView:UITableView) {
+		self.tableView = tableView
+	}
+
+	public init(with collectionView:UICollectionView) {
+		self.collectionView = collectionView
+	}
+
+	public func didUpdateData<M, VM, Q>(_ arrayViewModel: ArrayViewModel<M, VM, Q>,
+										_ update: Update)
+		where M : Equatable, VM : ViewModel<M>, Q : Query {
+
+			switch update {
+			case .reload:
+				tableView?.reloadData()
+				collectionView?.reloadData()
+			case .append(let indexes):
+				let indexPaths = indexes.map { IndexPath(row: $0, section: section) }
+				tableView?.insertRows(at: indexPaths, with: .automatic)
+				collectionView?.insertItems(at: indexPaths)
+			case .update(let indexes):
+				let indexPaths = indexes.map { IndexPath(row: $0, section: section) }
+				tableView?.reloadRows(at: indexPaths, with: .automatic)
+				collectionView?.reloadItems(at: indexPaths)
+			case .delete(let indexes):
+				let indexPaths = indexes.map { IndexPath(row: $0, section: section) }
+				tableView?.deleteRows(at: indexPaths, with: .automatic)
+				collectionView?.deleteItems(at: indexPaths)
+			case .move(_, _):
+				break
+			}
+	}
 }
